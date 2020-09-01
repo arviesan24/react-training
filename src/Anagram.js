25b3f1fb6db2c1d6e0091218000606f83dbd93c8
Romel Santoral <rsantoral@ph.indra.com>
2020-09-01 10:56:46 +0800

    Create Anagram component

---
+++ src/Anagram.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+class Anagram extends React.Component {
+  state = {
+    first: "",
+    second: "",
+    anagram: false,
+  }
+
+  anagramChecker = (e) => {
+    e.preventDefault();
+    let first = "";
+    let second = "";
+    first = this.state.first.split('').sort();
+    second = this.state.second.split('').sort();
+
+    if (first.join('')===second.join('')) {
+      this.setState({
+        ...this.state,
+        anagram: true
+      })
+    } else {
+      this.setState({
+        ...this.state,
+        anagram: false
+      })
+    }
+  }
+
+  firstHandler = (e) => {
+    this.setState({
+      ...this.state,
+      first: e.target.value
+    });
+  }
+  secondHandler = (e) => {
+    this.setState({
+      ...this.state,
+      second: e.target.value
+    });
+  }
+
+  render() {
+    console.log(this.state.anagram)
+    return (
+      <div>
+        <input type="text" value={this.state.first} onChange={this.firstHandler} />
+        <input type="text" value={this.state.second} onChange={this.secondHandler} />
+        <button onClick={this.anagramChecker}>Button</button>
+
+        <p>{this.state.anagram ? "Anagram" : "Not anagram"}</p>
+      </div>
+    );
+  }
+}
+
+
+export default Anagram;
